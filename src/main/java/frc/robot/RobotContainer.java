7180cc978dafa17bcd7a3ad55b447ab53dc34573
Astraea Robotics <ebfirstinc@gmail.com>
2025-12-04 18:41:23 -0500

    preparing for home test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,43 +4,20 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PS4Controller;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.CoralConstants.CoralStates;
 import frc.robot.Constants.DrivebaseConstants;
-import frc.robot.Constants.ElevatorConstants.ElevatorStates;
-import frc.robot.Constants.VisionConstants.AlignmentPosition;
-import frc.robot.commands.coral.*;
-// import frc.robot.commands.auto.paths.L1Mid;
-// import frc.robot.commands.auto.paths.LL1Side;
-// import frc.robot.commands.auto.paths.LL2Side;
-// import frc.robot.commands.auto.paths.RL1Side;
-// import frc.robot.commands.auto.paths.RL2Side;
-import frc.robot.commands.elevator.IncrementSetpoint;
-import frc.robot.commands.elevator.SetElevatorState;
 import frc.robot.commands.swerve.DriveRobotCentric;
 import frc.robot.commands.swerve.ResetGyro;
 import frc.robot.commands.swerve.TeleopSwerveNEW;
-import frc.robot.subsystems.CoralSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
-
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.commands.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -51,8 +28,6 @@ import com.pathplanner.lib.commands.*;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem();
-  private final CoralSubsystem m_coralSubsystem = new CoralSubsystem();
-  private final ElevatorSubsystem m_ElevatorSubsystem = new ElevatorSubsystem();
   private final VisionSubsystem m_VisionSubsystem = new VisionSubsystem();
 
   private final PS4Controller m_Controller = new PS4Controller(0);
@@ -84,70 +59,11 @@ public class RobotContainer {
   private final POVButton pov90 = new POVButton(m_Controller, 90);
   private final POVButton pov180 = new POVButton(m_Controller, 180);
   private final POVButton pov270 = new POVButton(m_Controller, 270);
-
-  SendableChooser<Command> chooser = new SendableChooser<>();
-
-  // private final Command m_L1Mid = new L1Mid(m_SwerveSubsystem, m_coralSubsystem, m_ElevatorSubsystem);
-  // private final Command m_RL1Side = new RL1Side(m_SwerveSubsystem, m_coralSubsystem, m_ElevatorSubsystem);
-  // private final Command m_LL1Side = new LL1Side(m_SwerveSubsystem, m_coralSubsystem, m_ElevatorSubsystem);
-  // private final Command m_RL2Side = new RL2Side(m_SwerveSubsystem, m_coralSubsystem, m_ElevatorSubsystem);
-  // private final Command m_LL2Side = new LL2Side(m_SwerveSubsystem, m_coralSubsystem, m_ElevatorSubsystem);
   
   private boolean isSlowModeOn = false; 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    NamedCommands.registerCommand("RaiseToL1", 
-      new ParallelCommandGroup(
-        new SetCoralState(m_coralSubsystem, CoralStates.kL1),
-        new SetElevatorState(m_ElevatorSubsystem, ElevatorStates.kCL1)
-      ));
-  
-    NamedCommands.registerCommand("RaiseToL2", 
-      new ParallelCommandGroup(
-        new SetCoralState(m_coralSubsystem, CoralStates.kL2),
-        new SetElevatorState(m_ElevatorSubsystem, ElevatorStates.kCL2)
-      ));
-  
-    NamedCommands.registerCommand("ScoreCoralL1", 
-      new ParallelDeadlineGroup(
-        new WaitCommand(1.0),
-        new ExtakeL1(m_coralSubsystem)
-      ));
-  
-    NamedCommands.registerCommand("ScoreCoral", 
-      new ParallelDeadlineGroup(
-        new WaitCommand(1.0),
-        new IntakeCoral(m_coralSubsystem, 5)
-      ));
-
-    NamedCommands.registerCommand("IntakeCoral",
-      new ParallelCommandGroup(
-        new WaitCommand(5),
-        new IntakeCoral(m_coralSubsystem, -5)
-      ));
-
-// og paths
-    // chooser.setDefaultOption("L1 Mid (OLD)", m_L1Mid);
-    // chooser.addOption("RL1 Side (OLD)", m_RL1Side);
-    // chooser.addOption("LL1 Side (OLD)", m_LL1Side);
-    // chooser.addOption("RL2 Side (OLD)", m_RL2Side);
-    // chooser.addOption("LL2Side (OLD)", m_LL2Side);
-  
-    // new pp paths
-    chooser.addOption("L1Mid", AutoBuilder.buildAuto("L1Mid"));
-    chooser.addOption("RL2", AutoBuilder.buildAuto("RL2"));
-    chooser.addOption("RL1", AutoBuilder.buildAuto("RL1"));
-    chooser.addOption("LL2", AutoBuilder.buildAuto("LL2"));
-    chooser.addOption("LL1", AutoBuilder.buildAuto("LL1"));
-
-    // 2 pc pp paths 
-    chooser.addOption("2Pc_RL1", AutoBuilder.buildAuto("2Pc_RL1"));
-    chooser.addOption("2Pc_RL2", AutoBuilder.buildAuto("2Pc_RL2"));
-    chooser.addOption("2Pc_LL1", AutoBuilder.buildAuto("2Pc_LL1"));
-    chooser.addOption("2Pc_LL2", AutoBuilder.buildAuto("2Pc_LL2"));
-  
-    SmartDashboard.putData("Auto choices", chooser);
 
     m_SwerveSubsystem.setDefaultCommand(new TeleopSwerveNEW(
       m_SwerveSubsystem,
@@ -173,10 +89,6 @@ public class RobotContainer {
 
     // Controller bindings
     kCross.onTrue(new ResetGyro(m_SwerveSubsystem));
-    
-    kR1.whileTrue(new IntakeCoral(m_coralSubsystem, -5));
-    kL1.whileTrue(new IntakeCoral(m_coralSubsystem, 5));
-    kTriangle.whileTrue(new ExtakeL1(m_coralSubsystem));
 
     kSquare.onTrue(new InstantCommand(() -> {
       isSlowModeOn = !isSlowModeOn;
@@ -187,21 +99,10 @@ public class RobotContainer {
     pov180.whileTrue(new DriveRobotCentric(m_SwerveSubsystem, DrivebaseConstants.kRobotCentricVel, 0));
     pov270.whileTrue(new DriveRobotCentric(m_SwerveSubsystem, 0, -DrivebaseConstants.kRobotCentricVel));
     pov90.whileTrue(new DriveRobotCentric(m_SwerveSubsystem, 0, DrivebaseConstants.kRobotCentricVel));
-
-    // Operator gamepad bindings 
-
-    kOperator1.onTrue(new ParallelCommandGroup(new SetElevatorState(m_ElevatorSubsystem, ElevatorStates.kRest), new SetCoralState(m_coralSubsystem, CoralStates.kRest))); // R
-    kOperator2.onTrue(new ParallelCommandGroup(new SetElevatorState(m_ElevatorSubsystem, ElevatorStates.kSource), new SetCoralState(m_coralSubsystem, CoralStates.kSource))); // SRC
-    kOperator3.onTrue(new ParallelCommandGroup(new SetElevatorState(m_ElevatorSubsystem, ElevatorStates.kCL1), new SetCoralState(m_coralSubsystem, CoralStates.kL1))); // CL1
-    kOperator4.onTrue(new ParallelCommandGroup(new SetElevatorState(m_ElevatorSubsystem, ElevatorStates.kCL2), new SetCoralState(m_coralSubsystem, CoralStates.kL2))); // Cl2
-    kOperator5.onTrue(new ParallelCommandGroup(new SetElevatorState(m_ElevatorSubsystem, ElevatorStates.kCL3), new SetCoralState(m_coralSubsystem, CoralStates.kL3))); // Cl3
-    kOperator6.onTrue(new IncrementSetpoint(m_ElevatorSubsystem, 1)); // IL
-    kOperator7.onTrue(new IncrementSetpoint(m_ElevatorSubsystem, -1)); // DL
-
   }
 
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return chooser.getSelected();
+    return null;
   }
 }
